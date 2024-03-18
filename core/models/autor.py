ae7e9fbc14c2dabd47a2688a7c79546e230b3e95
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-03-18 12:02:33 -0300

    Criação da API para Autor

---
+++ core/models/autor.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=40)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.email} {self.id} "
