afb06fa7e533a5e207623634f955250124243ddb
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-07-29 12:01:03 -0300

    conectando as tabelas categoria e editora a livros

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,10 +1,18 @@
 from django.db import models
+from .categoria import Categoria
+from .editora import Editora
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbt = models.CharField(max_length=32, blank=True, null=True)
     quantidade = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, blank=True, null=True)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=False
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
